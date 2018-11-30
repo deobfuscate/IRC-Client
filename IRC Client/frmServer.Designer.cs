e37f05b0548d463ce265e733855e94415d8ba903
Dan <33081566+ddmrz@users.noreply.github.com>
2018-11-29 18:38:03 -0800

    initial

---
+++ IRC Client/frmServer.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace IRC_Client
+{
+    partial class frmServer
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(584, 11);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(103, 20);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Connect";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.work);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(193, 11);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(277, 20);
+            this.textBox2.TabIndex = 3;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(476, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(102, 19);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Send";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webBrowser1.Location = new System.Drawing.Point(0, 0);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(709, 393);
+            this.webBrowser1.TabIndex = 5;
+            this.webBrowser1.DocumentCompleted += new System.Windows.Forms.WebBrowserDocumentCompletedEventHandler(this.webBrowser1_DocumentCompleted);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(556, 46);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(66, 41);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // frmServer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(709, 393);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.webBrowser1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button1);
+            this.Name = "frmServer";
+            this.Text = "IRC  Client";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ IRC Client/frmServer.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.ComponentModel;
+using System.IO;
+using System.Net.Sockets;
+using System.Windows.Forms;
+
+namespace IRC_Client{
+    public partial class frmServer : Form
+    {
+        private StreamWriter writer;
+        private static bool isConnected = false;
+        private const char PREFIX = '/';
+        public delegate void InvokeDelegate(string arg);
+
+        public frmServer()
+        {
+            InitializeComponent();
+            //Control.CheckForIllegalCrossThreadCalls = false;
+            //webBrowser1.Navigate("about:blank");
+            webBrowser1.DocumentText = "0";
+            webBrowser1.Document.OpenNew(true);
+            webBrowser1.Document.Write("<html><head><link rel='stylesheet' type='text/css' href='file://"+ Directory.GetCurrentDirectory() + "/styles.css'></head><body>\n");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            TcpClient irc = new TcpClient();
+            irc.BeginConnect("irc.quakenet.org", 6667, CallbackMethod, irc);
+        }
+
+        private void CallbackMethod(IAsyncResult ar)
+        {
+            webBrowser1.BeginInvoke(new InvokeDelegate(writeLine),"Connecting...");
+            try
+            {
+                isConnected = false;
+                TcpClient tcpclient = ar.AsyncState as TcpClient;
+
+                if (tcpclient.Client != null)
+                {
+                    tcpclient.EndConnect(ar);
+                    isConnected = true;
+                    
+                    NetworkStream stream = tcpclient.GetStream();
+                    StreamReader reader = new StreamReader(stream);
+                    writer = new StreamWriter(stream);
+                    string inputLine;
+                    writer.WriteLine("USER default 0 * :default");
+                    writer.Flush();
+                    writer.WriteLine("NICK UniqueNickname");
+                    writer.Flush();
+                    while(true)
+                    {
+                        while ((inputLine = reader.ReadLine()) != null)
+                        {
+                            //webBrowser1.BeginInvoke(new InvokeDelegate(writeLine), "<- " + inputLine + "\n");
+                            Console.WriteLine("<- " + inputLine);
+                            string[] splitInput = inputLine.Split(new Char[] { ' ' });
+                            if (splitInput.Length < 2)
+                                continue;
+                            if (splitInput[0] == "PING")
+                            {
+                                string key = splitInput[1];
+                                Console.WriteLine("-> PONG " + key);
+                                writer.WriteLine("PONG " + key);
+                                writer.Flush();
+                            }
+                            if (splitInput[1] == "NOTICE")
+                            {
+                                String tmp = "";
+                                for (int i = 2; i < splitInput.Length; i++)
+                                    tmp += " " + splitInput[i];
+                                tmp = tmp.TrimStart();
+                                if (tmp[0] == ':') tmp = tmp.Remove(0, 1);  //remove ':'
+                                writeLineA("-" + splitInput[0] + "- " + tmp);
+                            }
+                            if (inputLine.Length > 1)
+                            { 
+                                if (splitInput[1] == "001" | splitInput[1] == "002" | splitInput[1] == "003" | splitInput[1] == "004" | splitInput[1] == "005" | splitInput[1] == "251" | splitInput[1] == "252" | splitInput[1] == "254" | splitInput[1] == "255" | splitInput[1] == "005" | splitInput[1] == "375" | splitInput[1] == "372")
+                                {
+                                    string tmp = "";
+                                    for (int i = 3; i < splitInput.Length; i++)
+                                        tmp += " " + splitInput[i];
+                                    tmp = tmp.TrimStart();
+                                    if (tmp[0] == ':') tmp = tmp.Remove(0, 1);  //remove ':'
+                                    writeLineA("-Server- " + tmp);
+                                }
+                            }
+                        }
+                        writer.Close();
+                        reader.Close();
+                        tcpclient.Close();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                isConnected = false;
+                Console.WriteLine(ex);
+                writeLine("<- <span color='red'>DISCONNECTED</span>");
+            }
+        }
+
+        private void work(object sender, DoWorkEventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (textBox2.Text[0] == PREFIX)
+            {
+                String cmd = textBox2.Text.Remove(0, 1);
+                String[] parts = cmd.Split(' ');
+                switch (parts[0])
+                {
+                    case "server":
+                        TcpClient irc = new TcpClient();
+                        irc.BeginConnect(parts[1], 6667, CallbackMethod, irc);
+                        break;
+                    default:
+                        break;
+                }
+                textBox2.Clear();
+            }
+            else
+            {
+                writer.WriteLine(textBox2.Text);
+                writer.Flush();
+                Console.WriteLine("-> " + textBox2.Text);
+                textBox2.Clear();
+            }
+        }
+
+        private void webBrowser1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            //webBrowser1.Document.Write("<html><head><link rel='stylesheet' type='text/css' href='styles.css'></head><body>\n");
+        }
+        private void writeLine(string text)
+        {
+            webBrowser1.Document.Write(text+"<br>\n");
+            webBrowser1.Document.Window.ScrollTo(0, webBrowser1.Document.Body.ScrollRectangle.Height);
+        }
+        private void writeLineA(string text)
+        {
+            webBrowser1.BeginInvoke(new InvokeDelegate(writeLine), text);
+        }
+    }
+}
