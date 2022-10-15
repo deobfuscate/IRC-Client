2ee674eccb9d27c463007eb9ebf096f37426a9b6
Dan <33081566+deobfuscate@users.noreply.github.com>
2022-10-14 17:31:12 -0700

    Remove Program.cs

--- IRC Client/UI.cs
+++ IRC Client/UI.cs
@@ -402,4 +402,17 @@ namespace IRC_Client {
             Properties.Settings.Default.Save();
         }
     }
+
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main() {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new UI());
+        }
+    }
 }
